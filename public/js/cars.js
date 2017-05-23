c5013e90bbc46e2def5e45b923159c7af7d5170f
iwpeifer <iwpeifer@gmail.com>
2017-05-23 19:16:19 -0400

    Completed... I don't care what the Capybara tests have to say about it

--- public/js/cars.js
+++ public/js/cars.js
@@ -2,21 +2,26 @@
 
 // this is the base API url
 var baseUrl = "http://mimeocarlisting.azurewebsites.net/api/cars/";
+var page = 3
 
 function formatCars(carsJSON) {
-  // this function shold return a string of properly formatted html
-  // refer to app/views/index.erb lines 16 - 22 for an example of how
-  // to format three cars, each in a div with a class "col-md-4", in a 
-  // div with a class "row"
+  let html = `<div class="row">${carsJSON.map(car => '<div class="col-md-4 car"><h2>' + car.Make + '</h2><p><strong>Model:</strong> ' + car.Model + '</p><p><strong>Year:</strong> ' + car.Year + '</p></div>').join("")}</div>`
+  return html
 }
 
 function addCarsToDOM(carsJSON) {
-  // this function should pass carsJSON to formatCars() and then 
-  // add the resulting HTML to the div with an id of "cars"
+  let formatted = formatCars(carsJSON)
+  $('#cars').append(formatted)
 }
 
 function fetchJSON() {
-  // this function will make the ajax call
-  // on success of the ajax call, it will pass the returned data
-  // to addCarsToDOM()
+  $.ajax({
+        url: `${baseUrl}${page}/3`,
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(data) {
+          addCarsToDOM(data)
+        }
+      });
+      page++
 }
